35fcca412722050e9531edac8df979ebd700409b
Zo <960gd7@gmail.com>
2020-02-08 11:00:50 +0300

    show comment list

--- platform-react-js/src/components/CommentList.js
+++ platform-react-js/src/components/CommentList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Message} from "./Message";
-
+import moment from 'moment/moment';
 export class CommentList extends React.Component{
     render () {
 
@@ -14,7 +14,22 @@ export class CommentList extends React.Component{
 
         return (
                 <div className="card mb-3 mt-3 shadow-sm">
-                    Not done yet...
+                    {
+                        commentList && commentList.map((comment, comment_key) => {
+                            return (
+                                <div className="card-body" key={comment_key}>
+                                    <p className="card-text">
+                                        {comment.content}
+                                    </p>
+                                    <p className="card-text">
+                                        <small className="text-muted">
+                                            {moment(comment.published).fromNow()}
+                                        </small>
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             )
     }
